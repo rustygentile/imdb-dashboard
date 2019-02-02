ca3b7ba0f59b2ec521970e65c47a4dab9ad351b2
rustygentile <18237687+rustygentile@users.noreply.github.com>
2019-02-02 15:37:05 -0500

    refactored looper scripts, must haves loaded into AWS

---
+++ exploration/table_maker.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Text, DateTime, ForeignKey
+from sqlalchemy.orm import Session, relationship
+import os
+
+Base = declarative_base()
+
+# ORM of our SQL Database Tables
+class Series(Base):
+   __tablename__ = 'series'
+   id = Column(Integer, primary_key=True)
+   tconst = Column(Integer, index=True)
+   title = Column(String(255))
+   num_seasons = Column(Integer)
+   avg_rating = Column(Float)
+   num_votes = Column(Integer)
+   #episode = relationship("Episode", foreign_keys=[id, tconst]) #, back_populates="series")
+
+class Episode(Base):
+  __tablename__ = 'episodes'
+  id = Column(Integer, primary_key=True)
+  parent_id = Column(Integer, ForeignKey('series.id'))
+  parent_tconst = Column(Integer, ForeignKey('series.tconst'))
+  tconst = Column(Integer)
+  original_air_date = Column(DateTime)
+  number_votes = Column(Integer)
+  avg_rating = Column(Float)
+  season = Column(Integer)
+  episode = Column(Integer)
+  title = Column(String(255))
+  plot = Column(Text)
+  series_id = relationship("Series", foreign_keys=[parent_id])
+  series_tconst = relationship("Series", foreign_keys=[parent_tconst])
+
+# CREATE DATABASE connection
+def create_connection(make_tables=False):
+    password = os.environ['AWS_IMDB_PW']
+    user = 'masterblaster'
+    endpoint = 'imdb-explorer.clhfspuaimbp.us-east-1.rds.amazonaws.com'
+    args = "ssl_ca=../database/config/rds-ca-2015-us-east-1-root.pem"
+    rds_connection_string = f"{user}:{password}@{endpoint}/imdb_production?{args}"
+    engine = create_engine(f'mysql://{rds_connection_string}')
+    conn = engine.connect()
+    if make_tables:
+        Base.metadata.create_all(conn)
+    
+    return conn, Session(engine)

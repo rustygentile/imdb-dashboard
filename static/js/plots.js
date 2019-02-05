95bbfe583d34f5e40fe1b1502f7a23d4bf1577df
Minal J <minaljajoo0108@gmail.com>
2019-02-04 19:03:34 -0500

    updated index and plot.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,3 +1,4 @@
+
 //-----------------------------------------------
 // Update the page each time with new sample
 function update_plots() {
@@ -8,9 +9,8 @@ function update_plots() {
   console.log("listID: ");
   console.log(listID);
 
-  //var listShow = [listSerials.length,listSerials];
-  // var strShow = listShow.join();
   buildPlot(listID);
+  
 
 }
 //--------------- End of main functions ------------
@@ -32,45 +32,84 @@ function buildPlot(sample) {
 
   // Use `d3.json` to fetch the sample data for the plots
   d3.json("/plotdata/all_plots/" + sample).then(function (data) {
-    console.log(data);
-    console.log("#########");
-  
-   var trace = [];
-    var tracePlot = [];
+   
+   console.log("#########");
+ 
+
+   var traceBubble1 = [];
+    var traceBubble2 = [];
+    var xBar1 = [];
+    var yBar1 = [];
+    var traceLine1 = [];
+    var traceLine2 = [];
+    var firstDates =[];
+    var lastDates =[];
     for (var i = 0; i< data.length; i++){
      var showData = data[i].Episodes;
-      trace[i] = {
+     var seriesData = data[i].Series;
+     
+     console.log(seriesData);
+     traceBubble1[i] = {
         x: showData["original_air_date"],
         y: showData["rating"],
-        hovertext:showData["title"],
+        hovertext:showData["plot"],
         hoverinfo: {bordercolor: 'black'},
         color: "black",
         mode: 'markers'
         };
 
-        tracePlot[i] ={
-          x: showData["original_air_date"],
+        traceBubble2[i] ={
+        x: showData["original_air_date"],
         y: showData["votes"],
-        hovertext:showData["title"],
+        hovertext:showData["plot"],
         hoverinfo: {bordercolor: 'black'},
         color: "black",
         mode: 'markers'
-        };
+        };     
+        traceLine1[i] ={
+          x: seriesData["season_number"],
+          y: seriesData["season_avg_rating"],  
+          mode: 'lines+markers'
+          };  
+          traceLine2[i] ={
+            x: seriesData["season_number"],
+            y: seriesData["season_votes"],      
+            mode: 'lines+markers'
+            };  
 
-        }
-    
- 
-   var layout = layout_values('rating');
+          xBar1.push(seriesData["title"]);
+          yBar1.push(seriesData["number_of_seasons"]);
+          var dates = showData["original_air_date"];
+          firstDates.push(dates[0]);
+          lastDates.push(dates[dates.length-1]);
+      }
 
+   var layoutBubble1 = layout_values('Bubble Plot for shows: ','Original Air Date','Average Rating');
       var BUBBLE1 = document.getElementById('bubble1');
-      Plotly.newPlot(BUBBLE1,trace,layout);
+      Plotly.newPlot(BUBBLE1,traceBubble1,layoutBubble1);
 
-  var layout2 = layout_values('Votes');
+  var layoutBubble2 = layout_values('Bubble Plot for shows: ','Original Air Date','Number of Votes');
   var BUBBLE2 = document.getElementById('bubble2');
-      Plotly.newPlot(BUBBLE2,tracePlot,layout2);
+      Plotly.newPlot(BUBBLE2,traceBubble2,layoutBubble2);
 
+      var layoutLine1 =  layout_values('Line Chart for shows: ','Season Numbers','Average Rating per Season');
+      var LINE1 = document.getElementById('line1');
+          Plotly.newPlot(LINE1,traceLine1,layoutLine1);
+
+          var layoutLine2 = layout_values('Line Chart for shows: ','Season Numbers','Average Votes per Season');
+      var LINE2 = document.getElementById('line2');
+          Plotly.newPlot(LINE2,traceLine2,layoutLine2);
+
+        traceBar1= {  x: xBar1,
+        y: yBar1,    
+        type: 'bar'
+      };
+      var layoutBar1 = get_bar_layout('#Seasons');
+      var BAR1 = document.getElementById('bar1');
+      Plotly.newPlot(BAR1, [traceBar1], layoutBar1);
   });
 }
+
 
 //-----------------
 function get_trace(tData){
@@ -84,9 +123,9 @@ function get_trace(tData){
     };
     return traceData;
 }
-function layout_values(yaxis)
-    {var layout2 ={
-      title: '<b>Bubble Plot:  </b>',
+function layout_values(title, xaxis, yaxis)
+    {var layoutVal ={
+      title: '<b> '+ title + listSerials+  '</b>',
       height: 600,
       width: 800,
   
@@ -98,9 +137,53 @@ function layout_values(yaxis)
           left: 10
         },
         hovermode: 'closest',
-        xaxis: { title: 'Date' },
+        xaxis: { title: xaxis },
         yaxis: {title: yaxis },
         
         paper_bgcolor: "rgba(0,0,0,0)"
       };
-    return layout2;}
+    return layoutVal;}
+//-----------------
+
+//-----------------
+function get_bar_layout(title){
+  var layoutBar = {
+    height: 600,
+      width: 800,
+  
+      margin: 
+        {
+          top: 10,
+          bottom: 10,
+          right: 10,
+          left: 10
+        },
+        paper_bgcolor: "rgba(0,0,0,0)",
+    title: 'Bar Chart',
+    xaxis: {tickfont: {
+        size: 14,
+        color: 'rgb(107, 107, 107)'
+      }},
+    yaxis: {
+      title: title,
+      titlefont: {
+        size: 16,
+        color: 'rgb(107, 107, 107)'
+      },
+      tickfont: {
+        size: 14,
+        color: 'rgb(107, 107, 107)'
+      }
+    },
+    legend: {
+      x: 0,
+      y: 1.0,
+      bgcolor: 'rgba(255, 255, 255, 0)',
+      bordercolor: 'rgba(255, 255, 255, 0)'
+    },
+    barmode: 'group',
+    bargap: 0.15,
+    bargroupgap: 0.1
+  };
+  return layoutBar;
+}
